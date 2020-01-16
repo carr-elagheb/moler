7d83f571b4fe6f25528fc9e3ebbebb1ca54338ca
elagheb carr <elagheb.carr@nokia.com>
2020-01-16 10:17:43 +0100

    Detach AT-based command

--- moler/cmd/at/check_attach.py
+++ moler/cmd/at/check_attach.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-AT+CGATT=0 . detach
+AT+CGATT? . CheckAttach
 
 AT commands specification:
 https://portal.3gpp.org/desktopmodules/Specifications/SpecificationDetails.aspx?specificationId=1515
@@ -14,18 +14,19 @@ __email__ = 'elagheb.carr@nokia.com'
 from moler.cmd.at.at import AtCmd, AtCommandModeNotSupported
 
 
-class AtCmdDetach(AtCmd):
+class AtCmdCheckAttach(AtCmd):
     def __init__(self, connection=None, operation='execute'):
         """Create instance of AtCmdDetach class"""
-        super(AtCmdDetach, self).__init__(connection, operation)
+        super(AtCmdCheckAttach, self).__init__(connection, operation)
         if operation != 'execute':
             raise AtCommandModeNotSupported("{} operation no supported for: {}".format(operation, self))
-        self.set_at_command_string(command_base_string="AT+CGATT=0")
+        self.set_at_command_string(command_base_string="AT+CGATT?")
+        #TODO: fix timeout for check attach
         self.timeout = 180
 
     def parse_command_output(self):
         """
-        AT+CGATT=0
+        AT+CGATT=1
         OK
         """
         self.set_result({})
